747a4c18f1ff3d88f094bfa3236e1ee5aaceae29
Jaron Schulz <jaronchulz@gmail.com>
2019-04-08 19:59:39 +0200

    consume context with hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, createContext } from "react";
 import Toggle from "./components/Toggle";
 import useTitleInput from "./hooks/useTitleInput";
+
+export const UserContext = createContext();
 
 const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
   return (
-    <div className="main-wrapper" ref={ref}>
-      <h1
-        onClick={() => ref.current.classList.add("new-Fake-class")
-        }
-      >
-        Level Up Dishes
-      </h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-      <Toggle />
-    </div>
+    <UserContext.Provider
+      value={{
+        user: false
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add("new-Fake-class")}>
+          Level Up Dishes
+        </h1>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+        <Toggle />
+      </div>
+    </UserContext.Provider>
   );
 };
 
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from "../App";
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
+  const userInfo = useContext(UserContext);
+
+  if (!userInfo.user) return null;
+
   return (
     <div>
       <button onClick={() => setToggle(!isToggled)}>TOGGLE</button>
