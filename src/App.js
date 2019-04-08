c28f8064607e37eabf70c8671a654c16552ff2c6
Jaron Schulz <jaronchulz@gmail.com>
2019-04-08 19:24:02 +0200

    refactored classbased to functional with hooks - toggle, input etc.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Toggle from "./components/Toggle";
 
 const App = () => {
   // const [value, setValue] = useState(initialState);
@@ -7,10 +8,27 @@ const App = () => {
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
-      <h3>{name}</h3>
-      <input type="text" onChange={e => setName(e.target.value)} value={name} />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          formSubmit(name, setName);
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button>Submit</button>
+      </form>
+      <Toggle />
     </div>
   );
+};
+
+const formSubmit = (value, setValue) => {
+  console.log(`email sent to ${value}`);
+  setValue("");
 };
 
 export default App;
