a6fc0bac37e313fe158980196af40162b7bfd902
Jaron Schulz <jaronchulz@gmail.com>
2019-04-09 13:03:29 +0200

    useLayoutEffect when toggle is open lock screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, createContext } from "react";
+import React, { useRef, createContext, useEffect, useState } from "react";
 import Toggle from "./components/Toggle";
 import Counter from "./components/Counter";
 import useTitleInput from "./hooks/useTitleInput";
@@ -9,16 +9,32 @@ const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
 
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    const res = await fetch(
+      "https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes"
+    );
+    const data = await res.json();
+    setDishes(data);
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
+
   return (
     <UserContext.Provider
       value={{
-        user: false
+        user: true
       }}
     >
       <div className="main-wrapper" ref={ref}>
         <h1 onClick={() => ref.current.classList.add("new-Fake-class")}>
           Level Up Dishes
         </h1>
+        <Toggle>Toggle</Toggle>
+
         <form
           onSubmit={e => {
             e.preventDefault();
@@ -31,8 +47,18 @@ const App = () => {
           />
           <button>Submit</button>
         </form>
-        <Toggle />
-        <Counter />
+
+        {dishes.map(dish => (
+          <article className="dish-card dish-card--withImage">
+            <h3>{dish.name}</h3>
+            <p>{dish.desc}</p>
+            <div className="ingredients">
+              {dish.ingredients.map(ingredient => (
+                <span>{ingredient}</span>
+              ))}
+            </div>
+          </article>
+        ))}
       </div>
     </UserContext.Provider>
   );
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { UserContext } from "../App";
+import DishForm from "./DishForm";
 
 const Toggle = () => {
   const [isToggled, setToggle] = useState(false);
@@ -10,7 +11,7 @@ const Toggle = () => {
   return (
     <div>
       <button onClick={() => setToggle(!isToggled)}>TOGGLE</button>
-      {isToggled && <h2>Hello</h2>}
+      {isToggled && <DishForm />}
     </div>
   );
 };
